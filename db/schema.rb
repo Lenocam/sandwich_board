94fc703f72bc96e5be6b47f5f433af99d61e4e47
Reed MacConnell <Learn22Ruby@gmail.com>
2016-02-19 15:31:50 -0500

    need to stop for today, still have nomethoderror in sesssionscontroller#create undefined method digest

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160219032018) do
+ActiveRecord::Schema.define(version: 20160219195424) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 20160219032018) do
     t.string   "email"
     t.integer  "user_id"
     t.string   "password_digest"
+    t.string   "remember_digest"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
