a58ef00a724d201b4ab1eb57dd31fe90655c062c
Reed MacConnell <Learn22Ruby@gmail.com>
2016-02-22 12:14:17 -0500

    Finished log in/log out

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,13 @@ class User < ActiveRecord::Base
 	has_many :galleries
 	has_many :images
 
+	 # Returns the hash digest of the given string.
+	def User.digest(string)
+		cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+																									BCrypt::Engine.cost
+		BCrypt::Password.create(string, cost: cost)
+	end
+
 	# Returns a random token.
 	def User.new_token
 		SecureRandom.urlsafe_base64
