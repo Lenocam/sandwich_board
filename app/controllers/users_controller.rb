db40ef74e71962a86017ccdc943d26ce3a4e1420
Reed MacConnell <Learn22Ruby@gmail.com>
2016-02-26 16:10:35 -0500

    Added account activations via email

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,6 @@ class UsersController < ApplicationController
 	before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
 	before_action :correct_user,   only: [:edit, :update]
 	before_action :admin_user, only: :destroy
-	#before_action :set_user, only: [:show, :edit, :update, :destroy]
 
 	# GET /users
 	# GET /users.json
@@ -32,7 +31,7 @@ class UsersController < ApplicationController
 	def create
 		@user = User.new(user_params)
 		if @user.save
-			UserMailer.account_activation(@user).deliver_now
+			@user.send_activation_email
 			flash[:info] = "Please check your email to activate your account."
 			redirect_to root_url
 		else
