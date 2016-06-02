6a2beffbcf8d14b4f7635806414913cc555710fd
Reed MacConnell <Learn22Ruby@gmail.com>
2016-06-02 17:12:50 -0400

    Brainstorm approach/initial commit for branch

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -6,6 +6,7 @@ class AccountActivationsController < ApplicationController
       log_in user
       flash[:success] = 'Account activated!'
       redirect_to user
+      # can I add a redirect to a one time use only page here after inital activation.
     else
       flash[:danger] = 'Invalid activation link'
       redirect_to root_url
